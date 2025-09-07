046c323397dec371712eedc42e0a0b38439e6018
Hüseyin Özkan <huseyinozkan75@gmail.com>
2025-09-07 19:51:11 +0300

    ilk commit

--- Konu05Metotlar/Program.cs
+++ Konu05Metotlar/Program.cs
@@ -17,7 +17,8 @@
             int sayi3 = sayi1 + sayi2;
         }
 
-        static int  ToplamaYap(int sayi1, int sayi2, int sayi3) {
+        static int  ToplamaYap(int sayi1, int sayi2, int sayi3) 
+        {
             return sayi1 + sayi2 + sayi3;
         }
     }
