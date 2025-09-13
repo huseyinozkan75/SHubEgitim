751035b0e74cdca2272621b96155ba295bed6c41
Hüseyin Özkan <huseyinozkan75@gmail.com>
2025-09-14 00:07:17 +0300

    Commit

--- Konu05Metotlar/Program.cs
+++ Konu05Metotlar/Program.cs
@@ -10,6 +10,21 @@
             ToplamaYap(10, 20);
             int sonuc = ToplamaYap(10, 20, 30);
             Console.WriteLine("Hesaplanmış Fiyat: " + sonuc);
+
+            Console.WriteLine();
+            Console.WriteLine("Mail adresiniz: ");
+            var email = Console.ReadLine();
+
+            var mailGonderildimi = MailGonder(email);
+
+            if (mailGonderildimi == true)
+            {
+                Console.WriteLine("Mail gönderildi.");
+            }
+            else
+            {
+                Console.WriteLine("Mail gönderilemedi.");
+            }
         }
 
         static void ToplamaYap(int sayi1, int sayi2)
@@ -20,6 +35,16 @@
         static int  ToplamaYap(int sayi1, int sayi2, int sayi3) 
         {
             return sayi1 + sayi2 + sayi3;
+        }
+
+        static bool MailGonder(string mailAdresi)
+        {
+            if (!string.IsNullOrWhiteSpace(mailAdresi))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
---
+++ Konu06Diziler/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Konu06Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu06Diziler");
+
+            // Dizi Oluşturma
+            // int[]  sayilar;
+            int[] ogrenciler = new int[6]; // 6 elemanlı bir dizi  
+            ogrenciler[0] = 100;
+            ogrenciler[1] = 200;
+            ogrenciler[2] = 300;
+            ogrenciler[3] = 400;
+            ogrenciler[4] = 500;
+            ogrenciler[5] = 600;
+
+            Console.WriteLine("ogrenciler[5]: " + ogrenciler[5]);
+            ogrenciler[5] = 700;
+            Console.WriteLine("ogrenciler[5]: " + ogrenciler[5]);
+
+            string[] isimler = new string[5]; // 5 elemanlı bir dizi
+            isimler[0] = "Ahmet";
+            isimler[1] = "Mehmet";
+            isimler[2] = "Ayşe";
+            isimler[3] = "Fatma";
+            isimler[4] = "Zeynep";
+
+            Console.WriteLine("isimler[4]: " + isimler[4]);
+            Console.WriteLine();
+
+            int[] ogrenciler2 = { 90, 80, 70, 60, 50 }; // 5 elemanlı bir dizi
+            Console.WriteLine();
+
+            Console.WriteLine("Seçilen öğrenci no: " + ogrenciler2[3]);
+            ogrenciler2[3] = 550;
+            Console.WriteLine("Seçilen öğrenci no: " + ogrenciler2[3]);
+
+            string[] kategoriler = { "Bilgisayar", "Elektronik", "Cep Telefonu", "Beyaz Eşya", "Kitap" };
+            Console.WriteLine("kategoriler1" + kategoriler[1]);
+            kategoriler[1] = "Elektrikli Ev Aletleri";
+            Console.WriteLine("kategoriler1" + kategoriler[1]);
+            Console.WriteLine();
+        }
+    }
+}
