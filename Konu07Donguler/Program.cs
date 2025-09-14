7107417c95d6f0113f5f2bd66d40dae812c932f3
Hüseyin Özkan <huseyinozkan75@gmail.com>
2025-09-14 09:59:01 +0300

    Sınuflar

---
+++ Konu07Donguler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.WebSockets;
+
+namespace Konu07Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu07 Döngüler");
+            Console.WriteLine("1- For döngüsü:");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"i değişkeninin değeri : {i} ");
+            }
+
+            Console.WriteLine("2- While döngüsü:");
+            int j = 0;
+            while (j < 5)
+            {
+                Console.WriteLine($"j değişkeninin değeri : {j} ");
+                j++;
+            }
+
+            Console.WriteLine("3- Do & While döngüsü:");
+            int toplam = 7;
+            do {
+                Console.WriteLine($"toplam değişkeninin değeri : {toplam}");
+                toplam--;
+            } while (toplam < 5) ;
+
+
+
+            Console.WriteLine("3-Foreach döngüsü");
+            string[] kategoriler = { "Bilgisayar", "Telefon", "Tablet" };
+            foreach (var item in kategoriler)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Konu08SiniflarClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Konu08SiniflarClasses
+{
+    internal class Program
+    {
+
+        internal class Ev
+        {
+            // Sınıf içerisindeki değişkenler
+            internal string sokakAdi;
+            internal int kapiNo;
+        }
+
+        public class deneme
+        {
+            // Sınıf içerisindeki değişkenler
+            public string urunAdi = "public oge herkes erişebilir.";
+            protected class test
+            {
+                // Sınıf içerisindeki değişkenler
+                string urunAdi= "";
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu08Sınıflar Classes");
+            /*
+             Erişim belirteçleri 4 ana sınıfa ayrılır.
+                1- Public : Erişim kısıtı yoktur. Her yerden erişilebilir.
+                2- Private : Sadece tanımlandığı sınıf içerisinden erişilebilir.
+                3- Internal : Sadece bulunduğu proje içerisinden erişilebilir.
+                4- Protected : Sadece tanımlandığı sınıf ve o sınıftan türeyen sınıflardan erişilebilir.
+
+
+             */
+
+            Ev ilkEV = new Ev();
+            ilkEV.sokakAdi = "Yıldız";
+            ilkEV.kapiNo = 34;
+            Console.WriteLine($"İlkEv sokak adi = {ilkEV.sokakAdi}");
+            Console.WriteLine($"İlkEv kapi no = {ilkEV.kapiNo}");
+        }
+    }
+}
